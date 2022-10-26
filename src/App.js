5766e416e2cbe13a5b7fc9ea7f4019291957dbeb
Aryan <singh.aryan837@gmail.com>
2022-10-26 11:30:29 +0530

    Solutions added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import {useEffect,useState} from 'react';
 function App() {
-  const [solution,setSolution]=useState(null);
-useEffect(()=>{
- fetch('http://localhost:3001/solutions')
- .then(res=>res.json())
- .then(json => {
-  console.log(json)
- })
-},[])
+  const [solution, setSolution] = useState(null)
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/solutions')
+      .then(res => res.json())
+      .then(json => {
+        // random int between 0 & 14
+        const randomSolution = json[Math.floor(Math.random()*json.length)]
+        setSolution(randomSolution.word)
+      })
+  }, [setSolution])
   return (
     <div className="App">
       <h1>WordIT</h1>
+      {solution && <div>Solution is :{solution}</div>}
+      {/* This is done to ensure that only when solution is present then the output is printed */}
+
     </div>
   );
 }
